278a16dadea9aa42fde0d1c503fe9e583cc87865
PCSOFIYAN\sofiy <ifren.sofiyan@gmail.com>
2018-05-21 13:59:16 +0200

    Fonctionnalités répondant au besoin finalisées. TODO: - Améliorations de rendu (item selected, ouleurs, etc) - Gestion de l'affichage si il n'y a pas de résultats à la recherche

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { SearchComponent } from './search/search.component';
-import { ArtistsDetailsComponent } from './artists-details/artists-details.component';
 import { AppServiceService } from './services/app-service.service';
 import { HttpModule, ConnectionBackend } from '@angular/http';
 import { FormsModule, ReactiveFormsModule, FormGroup } from '@angular/forms';
-import { ArtistInformationsComponent } from './artist-informations/artist-informations.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    SearchComponent,
-    ArtistsDetailsComponent,
-    ArtistInformationsComponent
+    SearchComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +22,6 @@ import { ArtistInformationsComponent } from './artist-informations/artist-inform
   ],
   providers: [
     AppServiceService,
-    ArtistsDetailsComponent,
     SearchComponent
   ],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AppServiceService } from '../services/app-service.service';
 
 @Component({
   selector: 'app-search',
@@ -8,20 +9,40 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  
   myToken: string;
   myArtist: string;
 
-  constructor() { 
+  artistsNumber: JSON;
+  albumsNumber: JSON;
+
+  constructor(private appService: AppServiceService) { 
   }
 
-// Fonction appelée lors de la validation du formulaire :  récupération des données et initialisation des variables globales 
-onValidate(form: NgForm){
-  this.myToken = form.value.token;
-  this.myArtist = form.value.artist;
-  console.log(this.myToken);
-  console.log(this.myArtist);
-}
+  // Fonction appelée lors de la validation du formulaire :  
+  // récupération des données et initialisation des variables globales 
+  onValidate(form: NgForm){
+    this.myToken = form.value.token;
+    this.myArtist = form.value.artist;
+
+    // Appel au service (Controller) avec les valeurs du formulaire
+    this.appService.searchArtist(this.myToken, this.myArtist, 'artist').subscribe(response => {
+      // on donne à la variable accessible depuis le template le nombre d'items de la recherche
+      this.artistsNumber = response.artists.items; 
+    });
+  }
+
+  // TODO : Afficher les albums de l'artiste sélectionné dans un nouveau composant
+  goToInformations(id: string){
+    let token = this.myToken;
+
+    // Appel au service (Controller) avec l'identifiant de l'artiste sélectionné
+    this.appService.getArtist(token, id).subscribe(response => {
+      // on donne à la variable accessible depuis le template le nombre d'items de la recherche
+      this.albumsNumber = response.items; 
+    })
+
+  }
 
   ngOnInit() {
   }
